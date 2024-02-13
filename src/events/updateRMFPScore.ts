c1c15fc09e55494064730d4aed299d282908d7e7
Gabriel Britain <quontas@gmail.com>
2024-02-13 12:30:25 -0600

    Check for emoji equality, and fetch partials

--- src/events/updateRMFPScore.ts
+++ src/events/updateRMFPScore.ts
@@ -2,6 +2,18 @@ import type { MessageReaction, PartialMessageReaction, PartialUser, User } from 
 import { prisma } from '../common/prisma.js';
 
 export const updateRMFPScore = async (reaction: MessageReaction | PartialMessageReaction, user: PartialUser | User) => {
+	if (reaction.emoji.name !== '❤️') {
+		return;
+	}
+
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Error fetching full information for reaction:', error);
+		}
+	}
+
 	const matchingEntry = await prisma.entry.findUnique({
 		where: {
 			messageId: reaction.message.id,
@@ -17,7 +29,7 @@ export const updateRMFPScore = async (reaction: MessageReaction | PartialMessage
 			messageId: reaction.message.id,
 		},
 		data: {
-			reacts: reaction.count!,
+			reacts: reaction.count ?? 0,
 		},
 	});
 };
