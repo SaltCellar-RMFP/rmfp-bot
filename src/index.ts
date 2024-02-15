1fa4edd4d33bda3f651a62e08bd33ba5e1cb8e07
Eashwar Mohan <eashwar@umich.edu>
2024-02-14 21:52:37 -0800

    workflow

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
 import process from 'node:process';
 import { URL } from 'node:url';
-import { Client, GatewayIntentBits, IntentsBitField, Partials } from 'discord.js';
 import { loadCommands, loadEvents } from './util/loaders.js';
 import { registerEvents } from './util/registerEvents.js';
 
@@ -16,13 +16,13 @@ const client = new Client({
 	partials: [Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildScheduledEvent],
 });
 
-// Load the events and commands
+// Load the events and commands.
 const eventsUrl = new URL('events/', import.meta.url);
 const commandsUrl = new URL('commands/', import.meta.url);
 const events = await loadEvents(eventsUrl);
 const commands = await loadCommands(commandsUrl);
 
-// Register the event handlers
+// Register the event handlers!!
 registerEvents(commands, events, client);
 
 // Login to the client
